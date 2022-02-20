72bd20ea8b1f3f83233efd9f1b79d7c487ce1f4c
FFrankOzz <37096255+ffrankozz@users.noreply.github.com>
2022-02-20 21:13:23 +0300

    Order list changed

--- OrdersApi/Program.cs
+++ OrdersApi/Program.cs
@@ -15,8 +15,9 @@ app.MapGet("/orders", () =>
 {
     var orders = new List<Order>
     {
-        new Order(1, new DateTime(2021, 11, 01)),
-        new Order(2, new DateTime(2021, 11, 02)),
+        new Order(1, new DateTime(2022, 12, 01)),
+        new Order(2, new DateTime(2022, 12, 02)),
+        new Order(3, new DateTime(2022, 3, 03)),
     };
     return orders;
 })
